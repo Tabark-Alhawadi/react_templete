e2d42ec78d883b62d1a706516b5846329ff5929f
TabarkAlhawadi <tabarkalhawadi1@gmail.com>
2023-02-26 19:30:47 +0300

    template

---
+++ template/src/App.js
@@ -0,0 +1,17 @@
+
+import Home from './pages/home';
+import Register from './pages/register';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import EditProfile from './pages/EditProfile';
+import Chat from './pages/Chat';
+
+function App() {
+  return (
+    <div >
+     <Chat/>
+    </div>
+  );
+}
+
+export default App;
